7473d12cb5c62d3af829201dc74c988ec6489ea8
Vismaya <vismayava6@gmail.com>
2026-04-03 11:20:30 +0530

    branch

--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,2 @@
-// ✅ config.js
-export const API_BASE_URL = "https://vsaverbackend.imcbs.com/api";
+export const API_BASE_URL = "http://192.168.1.20:8000/api";
+// const PROD_API = "https://vsaverbackend.imcbs.com/api";
