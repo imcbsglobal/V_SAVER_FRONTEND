c180622da9b39d8565d16fb03d52003354dd5f44
Vismaya <vismayava6@gmail.com>
2026-04-04 09:47:06 +0530

    Update API_BASE_URL export comment and retain PROD_API definition

--- src/services/config.js
+++ src/services/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://192.168.1.20:8000/api";
-// const PROD_API = "https://vsaverbackend.imcbs.com/api";
+// export const API_BASE_URL = "http://192.168.1.20:8000/api";
+const PROD_API = "https://vsaverbackend.imcbs.com/api";
